82610f5108f27f40aa953ca410488e53dec92bb5
pedrocosta <pedrohenriquecs012@gmail.com>
2026-06-01 09:58:45 -0300

    Esqueleto inicial montado, CSS adicionado

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#input")
+
+function megaS(num){
+  if (num < 6 || num > 9){
+    console.log("Coloque um número de 6 a 9")
+    return arr = [];
+  } else {
+    
+  }
+}
